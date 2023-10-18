a9f52e3366b8135d5312180ebe016754a5edb459
filipgalic <12galicfilip@gmail.com>
2023-10-18 14:15:15 -0400

    Modified the style of the HTML

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ document.getElementById("myBtn").addEventListener("click", function () {
 });
 
 
-
 function makeGraph() {
   const svg = d3.select("svg"),
     width = +svg.attr("width"),
@@ -48,34 +47,77 @@ function makeGraph() {
     .range([50, width - 50]);
 
   const yScale = d3.scaleLinear()
-    .domain([d3.min(data, d => d.y), d3.max(data, d => d.y)])
+    .domain([0, d3.max(data, d => d3.max(d.y))])
     .range([height - 50, 50]);
 
-  svg.selectAll(".dot")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("class", "dot")
-    .attr("cx", d => xScale(d.x))
-    .attr("cy", d => yScale(d.y))
-    .attr("r", 1.25)
-    .on("mouseover", function (event, d) { 
-      tooltip.style("visibility", "visible")
-        .text("Model: " + d.model)
-        .style("top", (event.pageY - 10) + "px")
-        .style("left", (event.pageX + 10) + "px");
-    })
-    .on("mouseout", function () {
-      tooltip.style("visibility", "hidden");
-    });
+  // Group the data by model name
+  const groupedData = d3.group(data, d => d.model);
+
+  // Give each model a unique colour
+  const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+  // Line generator for step line chart
+  const line = d3.line()
+    .x(d => xScale(d.x))
+    .y(d => yScale(d.y))
+    .curve(d3.curveStepAfter);
+
+  const lineThickness = 4;
+
+  groupedData.forEach((group, modelName) => {
+    svg.append("path")
+      .datum(group)
+      .attr("class", "line")
+      .attr("d", line)
+      .attr("fill", "none")
+      .attr("stroke", colorScale(modelName))
+      .attr("stroke-width", lineThickness)
+      .on("mouseover", function (event, d) {
+        tooltip.style("visibility", "visible")
+          .text("Model: " + modelName)
+          .style("top", (event.pageY - 10) + "px")
+          .style("left", (event.pageX + 10) + "px");
+      })
+      .on("mouseout", function () {
+        tooltip.style("visibility", "hidden");
+      });
+  });
+
+
+let yLegendSection = 65;  
+
+//legend
+const legend = svg.append('g')
+    .attr('class', 'legend');
+
+groupedData.forEach((group, modelName) => {
+    const legendSection = legend.append('g')
+        .attr('transform', `translate(0, ${yLegendSection})`);
+
+    legendSection.append('rect')
+        .attr('x', 780
+      )
+        .attr('y', 0)
+        .attr('width', 20)
+        .attr('height', 20)
+        .attr('fill', colorScale(modelName));
+
+    //model name
+    legendSection.append('text')
+        .attr('x', 800
+       + 15)
+        .attr('y', 15)
+        .text(modelName);
+
+    yLegendSection += 25;
+});
+
 
   svg.append("g")
     .attr("transform", "translate(0," + (height - 40) + ")")
     .call(d3.axisBottom(xScale));
 
-
   svg.append("g")
     .attr("transform", "translate(40, 0)")
     .call(d3.axisLeft(yScale));
-
 }
