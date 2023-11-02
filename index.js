e84c11c33bbf89d6b67eb61cfb96143545188a1f
waheebhashmi <waheebhashmi@gmail.com>
2023-11-01 20:10:13 -0400

    created new graph type (WIP)

--- index.js
+++ index.js
@@ -37,91 +37,99 @@ document.getElementById("myBtn").addEventListener("click", function () {
 
 
 function makeGraph() {
-  const svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
-  const tooltip = d3.select("#tooltip");
+    const svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+    const tooltip = d3.select("#tooltip");
 
-  const xScale = d3.scaleLinear()
-    .domain([d3.min(data, d => d.x), d3.max(data, d => d.x)])
-    .range([50, width - 50]);
-
-  const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d3.max(d.y))])
-    .range([height - 50, 50]);
-
-  // Group the data by model name
-  const groupedData = d3.group(data, d => d.model);
-
-  // Give each model a unique colour
-  const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-
-  // Line generator for step line chart
-  const line = d3.line()
-    .x(d => xScale(d.x))
-    .y(d => yScale(d.y))
-    .curve(d3.curveStepAfter);
-
-  const lineThickness = 4;
-
-  groupedData.forEach((group, modelName) => {
-    svg.append("path")
-      .datum(group)
-      .attr("class", "line")
-      .attr("d", line)
-      .attr("fill", "none")
-      .attr("stroke", colorScale(modelName))
-      .attr("stroke-width", lineThickness)
-      .on("mouseover", function (event, d) {
-        tooltip.style("visibility", "visible")
-          .text("Model: " + modelName)
-          .style("top", (event.pageY - 10) + "px")
-          .style("left", (event.pageX + 10) + "px");
-      })
-      .on("mouseout", function () {
-        tooltip.style("visibility", "hidden");
-      });
-  });
+    const xScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d.x), d3.max(data, d => d.x)])
+        .range([50, width - 50]);
 
 
-let yLegendSection = 65;  
+    // Group the data by model name
+    const groupedData = d3.group(data, d => d.model);
 
-//legend
-const legend = svg.append('g')
-    .attr('class', 'legend');
-
-groupedData.forEach((group, modelName) => {
-    const legendSection = legend.append('g')
-        .attr('transform', `translate(0, ${yLegendSection})`);
-
-    legendSection.append('rect')
-        .attr('x', 780
-      )
-        .attr('y', 0)
-        .attr('width', 20)
-        .attr('height', 20)
-        .attr('fill', colorScale(modelName));
-
-    //model name
-    legendSection.append('text')
-        .attr('x', 800
-       + 15)
-        .attr('y', 15)
-        .text(modelName);
-
-    yLegendSection += 25;
-});
+    // Give each model a unique colour
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
 
-  svg.append("g")
-    .attr("transform", "translate(0," + (height - 40) + ")")
-    .call(d3.axisBottom(xScale));
+    const lineThickness = 3;
 
-  svg.append("g")
-    .attr("transform", "translate(40, 0)")
-    .call(d3.axisLeft(yScale));
-<<<<<<< HEAD
+    let yOffset = 50;
+    const yOffsetStep = height / groupedData.size - 100;
+
+    groupedData.forEach((group, modelName) => {
+
+        const yScale = d3.scaleLinear()
+            .domain([0, 5]) // Fixed domain from 0 to 5
+            .range([yOffset + yOffsetStep, yOffset]);
+
+
+        const line = d3.line()
+            .x(d => xScale(d.x))
+            .y(d => yScale(d.y))
+            .curve(d3.curveStepAfter);
+
+
+        svg.append("path")
+            .datum(group)
+            .attr("class", "line")
+            .attr("d", line)
+            .attr("fill", "none")
+            .attr("stroke", colorScale(modelName))
+            .attr("stroke-width", lineThickness)
+            .on("mouseover", function (event, d) {
+                tooltip.style("visibility", "visible")
+                  .text("Model: " + modelName)
+                  .style("top", (event.pageY - 10) + "px")
+                  .style("left", (event.pageX + 10) + "px");
+              })
+              .on("mouseout", function () {
+                tooltip.style("visibility", "hidden");
+              });
+
+        // Y-axis for each model
+        svg.append("g")
+            .attr("transform", `translate(50,${2})`)
+            .call(d3.axisLeft(yScale).tickValues([0, 1, 2, 3, 4]));
+
+
+        yOffset += yOffsetStep;
+    });
+
+
+    svg.append("g")
+        .attr("transform", "translate(0," + (height - 450) + ")")
+        .call(d3.axisBottom(xScale));
+
+
+    // Legend
+    let yLegendSection = 1;
+    const legend = svg.append('g')
+        .attr('class', 'legend');
+
+    groupedData.forEach((group, modelName) => {
+        const legendSection = legend.append('g')
+            .attr('transform', `translate(0, ${yLegendSection})`);
+
+        legendSection.append('rect')
+            .attr('x', 780
+            )
+            .attr('y', 0)
+            .attr('width', 20)
+            .attr('height', 20)
+            .attr('fill', colorScale(modelName));
+
+        //model name
+        legendSection.append('text')
+            .attr('x', 800 
+            + 15)
+            .attr('y', 15)
+            .text(modelName);
+
+        yLegendSection += 25;
+    });
 }
-=======
-}
->>>>>>> origin/html_modified
+
+
